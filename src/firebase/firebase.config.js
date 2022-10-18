09dea78a5f1735632c25e52e4fa1521c461ad54a
dev-sajjad <sajjadbmarpc@gmail.com>
2022-10-18 23:34:24 +0600

    firebase initialized

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKQwx79uOX0ctcfFtyEjXVOkVeQK5eP6E",
+  authDomain: "auth-router-context-app-6b412.firebaseapp.com",
+  projectId: "auth-router-context-app-6b412",
+  storageBucket: "auth-router-context-app-6b412.appspot.com",
+  messagingSenderId: "447012231261",
+  appId: "1:447012231261:web:db079d9053c326d3d855b4",
+};
+
+
+const app = initializeApp(firebaseConfig)
+
+export default app;
